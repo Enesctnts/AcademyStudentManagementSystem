e6bce1ad1bf209d6723988f19ec843c67ea399d7
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 12:06:43 +0300

    Generic Repository yazıldı.

---
+++ ASMSDataAccessLayer/ContactsDAL/IRepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ContactsDAL
+{
+    public interface IRepositoryBase<T,Id> where T : class,new()
+    {
+
+        //includeEntities inner join ile ilişkide oldugu entity getirecektir.
+        IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null,Func<IQueryable<T>, IOrderedQueryable<T>> orderBy=null, string includeEntities = null);
+
+        //alttaki filter = null yap hata almak için böyle yapıyoz :((
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter=null, string includeEntities=null);
+        T GetById(Id id);
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+
+    }
+}
