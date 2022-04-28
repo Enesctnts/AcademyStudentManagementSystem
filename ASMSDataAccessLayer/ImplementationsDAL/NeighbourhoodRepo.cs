fa770d37b0ca46ad86fa1ca6dedc394790a84999
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 14:49:00 +0300

    NeighbourhoodRepo oluşturuldu. Generic Repository'den implementasyonu yapıldı.

---
+++ ASMSDataAccessLayer/ImplementationsDAL/NeighbourhoodRepo.cs
@@ -0,0 +1,19 @@
+﻿using ASMSDataAccessLayer.ContactsDAL;
+using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ImplementationsDAL
+{
+    public class NeighbourhoodRepo : RepositoryBase<Neighbourhood, int>, INeighbourhoodRepo
+    {
+        public NeighbourhoodRepo(MyContext myContext) : base(myContext)
+        {
+
+        }
+    
+    }
+}
