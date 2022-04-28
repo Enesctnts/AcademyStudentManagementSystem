dbb3ddb291bcb596eab8aee3cf20f831ff999b58
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 14:49:18 +0300

    StudentAttendanceRepo oluşturuldu. Generic Repository'den implementasyonu yapıldı.

---
+++ ASMSDataAccessLayer/ImplementationsDAL/StudentAttendanceRepo.cs
@@ -0,0 +1,19 @@
+﻿using ASMSDataAccessLayer.ContactsDAL;
+using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ImplementationsDAL
+{
+    public class StudentAttendanceRepo : RepositoryBase<StudentAttendance, int>, IStudentAttendanceRepo
+    {
+        public StudentAttendanceRepo(MyContext myContext) : base(myContext)
+        {
+
+        }
+    
+    }
+}
