b2ec635de21271561dfc555296fc57c57e0148b8
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 14:48:07 +0300

    CourseRepo oluşturuldu. Generic Repository'den implementasyonu yapıldı.

---
+++ ASMSDataAccessLayer/ImplementationsDAL/CourseGroupRepo.cs
@@ -0,0 +1,18 @@
+﻿using ASMSDataAccessLayer.ContactsDAL;
+using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ImplementationsDAL
+{
+    public class CourseGroupRepo : RepositoryBase<CourseGroup, int>, ICourseGroupRepo
+    {
+        public CourseGroupRepo(MyContext myContext) : base(myContext)
+        {
+
+        }
+    }
+}
