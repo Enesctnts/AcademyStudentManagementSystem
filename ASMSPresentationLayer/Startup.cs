add6b93839084f4083ef3cd351964f9cf9ee9af6
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 09:31:40 +0300

    Add project files.

---
+++ ASMSPresentationLayer/Startup.cs
@@ -0,0 +1,82 @@
+using ASMSDataAccessLayer;
+using ASMSEntityLayer.IdentityModels;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASMSPresentationLayer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Aspnet Core'un ConnectionString baðlantýsý yapabilmesi için 
+            //yapýlandýrma servislerine dbcontext nesnesini eklemesi gerekir
+            services.AddDbContext<MyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
+
+
+            services.AddControllersWithViews();
+            services.AddRazorPages(); // razor sayfalarý için
+            services.AddMvc();
+            services.AddSession(options =>
+                options.IdleTimeout = TimeSpan.FromSeconds(20));
+            // oturum zamaný
+
+            //**************************************//
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequiredLength = 3;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_@.";
+            }).AddDefaultTokenProviders().AddEntityFrameworkStores<MyContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles(); // wwwroot klasörünün eriþimi içindir.
+            app.UseRouting(); // Controller/Action/Id 
+            app.UseSession(); // Oturum mekanizmasýnýn kullanýlmasý için
+            app.UseAuthorization(); // [Authorize] attribute için (yetki)
+            app.UseAuthentication(); // Login Logout iþlemlerinin gerektirtiði oturum iþleyiþlerini kullanabilmek için.
+         
+            //MVC ile ayný kod bloðu endpoint'in mekanizmasýnýn nasýl olacaðý belirleniyor
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            
+        }
+    }
+}
