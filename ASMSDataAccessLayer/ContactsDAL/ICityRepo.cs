5cd0ff11c287c70571d7c41fbbd45109b56e9760
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 12:49:08 +0300

    Generic Repository arayüzleri yazıldı.

---
+++ ASMSDataAccessLayer/ContactsDAL/ICityRepo.cs
@@ -0,0 +1,15 @@
+﻿using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ContactsDAL
+{
+    public interface ICityRepo : IRepositoryBase<City,byte>
+    {
+
+    }
+   
+}
