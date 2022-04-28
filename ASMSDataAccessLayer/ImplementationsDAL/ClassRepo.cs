1a9c52078eaf73949fa1fa0b0de46f3bd5d8d391
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 14:47:52 +0300

    ClassRepo oluşturuldu. Generic Repository'den implementasyonu yapıldı.

---
+++ ASMSDataAccessLayer/ImplementationsDAL/ClassRepo.cs
@@ -0,0 +1,19 @@
+﻿using ASMSDataAccessLayer.ContactsDAL;
+using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ImplementationsDAL
+{
+    public class ClassRepo:RepositoryBase<Class,int>,IClassRepo
+    {
+        public ClassRepo(MyContext myContext):base(myContext)
+        {
+
+        }
+
+    }
+}
