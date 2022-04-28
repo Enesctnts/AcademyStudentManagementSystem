7c08ca603e28ee8d7a8311d7062fdeabbd6ab6d7
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 14:47:45 +0300

    CityRepo oluşturuldu. Generic Repository'den implementasyonu yapıldı.

---
+++ ASMSDataAccessLayer/ImplementationsDAL/CityRepo.cs
@@ -0,0 +1,21 @@
+﻿using ASMSDataAccessLayer.ContactsDAL;
+using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ImplementationsDAL
+{
+    public class CityRepo : RepositoryBase<City,byte>,ICityRepo
+    {
+        //ICityRepo kalıtım 
+
+
+        public CityRepo(MyContext myContext):base(myContext)
+        {
+            // ctor oluşturmamızın sebebi kalıtım aldıgımız class'ın ctor'ında myContext istediği için
+        }
+    }
+}
