a3bf441f327704d86a7d0d69a12c1c0ba06454d5
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 15:16:37 +0300

    Desing patternlerden UnitOfWork ve Generic Repository projemize eklendi.

---
+++ ASMSDataAccessLayer/ContactsDAL/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ContactsDAL
+{
+    public interface IUnitOfWork
+    {
+        ICityRepo CityRepo { get;  }
+        IClassRepo ClassRepo { get;  }
+        ICourseGroupRepo CourseGroupRepo { get;  }
+        ICourseRepo CourseRepo { get;  }
+        IDistrictRepo DistrictRepo { get;  }
+        INeighbourhoodRepo NeighbourhoodRepo { get;  }
+        IStudentAttendanceRepo StudentAttendanceRepo { get;  }
+        IStudentRepo StudentRepo { get;  }
+        IStudentsCourseGroupRepo StudentsCourseGroupRepo { get;  }
+        ITeacherRepo TeacherRepo { get;  }
+        IUsersAddressRepo UsersAddressRepo { get;  }
+
+
+    }
+}
