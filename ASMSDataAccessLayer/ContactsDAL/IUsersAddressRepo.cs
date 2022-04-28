f2753ea5b0e58f0f9a37dcf920d372720732ea1a
Enes Çetintaş <enesctnts@gmail.com>
2022-04-28 12:50:27 +0300

    Generic Repository arayüzleri yazıldı.

---
+++ ASMSDataAccessLayer/ContactsDAL/IUsersAddressRepo.cs
@@ -0,0 +1,13 @@
+﻿using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ContactsDAL
+{
+    public interface IUsersAddressRepo  : IRepositoryBase<UsersAddress, int>
+    {
+    }
+}
