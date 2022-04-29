1c327d02d92ba0971a28d219deef97c98689efa0
Enes Çetintaş <enesctnts@gmail.com>
2022-04-29 12:41:28 +0300

    UsersAddressViewModel oluşturuldu.

---
+++ ASMSEntityLayer/ViewModels/UsersAddressVM.cs
@@ -0,0 +1,38 @@
+﻿using ASMSEntityLayer.IdentityModels;
+using ASMSEntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSEntityLayer.ViewModels
+{
+    public class UsersAddressVM
+    {
+        public string UserId { get; set; }
+        public int Id { get; set; }
+        public int NeighbourhoodId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Adres başlığı en az 2 en çok 50 karakter aralığında olmalıdır!!")]
+        public string AddressTitle { get; set; }
+
+        [StringLength(500, ErrorMessage = "Adres detayı en çok 500 karakter aralığında olabilir!!")]
+        public string AddressDetails { get; set; }
+
+        [StringLength(5, MinimumLength = 5, ErrorMessage = "Posta Kodu 5 karakter olmalıdır!!")]
+        public string PostCode { get; set; } 
+        public  AppUser AppUser { get; set; } //ViewModel oluşturulken virtual kullanılmaz normalde public virtual AppUser AppUser şeklindeydi ama viewmodelde kullanmıyoruz.
+
+        [Required(ErrorMessage ="Mahalle seçimi gereklidir.")]
+        public  Neighbourhood Neighbourhood { get; set; }
+
+        //ToDo: ??? Aşagıdakilerle il ve ilçeye ulaşabilir miyim?
+        public City City { get; set; }
+        public District District { get; set; }
+    }
+}
