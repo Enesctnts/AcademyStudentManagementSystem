cbfbc7e2f3907a35d299162ab7238d083c8fb2c2
Enes Çetintaş <enesctnts@gmail.com>
2022-04-29 12:36:20 +0300

    UsersAddressViewModel ve UsersAddress automapper yardımıyla birbirleri arasındaki dönüşüm yapıldı.

---
+++ ASMSEntityLayer/Mappings/Maps.cs
@@ -0,0 +1,31 @@
+﻿using ASMSEntityLayer.Models;
+using ASMSEntityLayer.ViewModels;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSEntityLayer.Mappings
+{
+    public class Maps : Profile
+    {
+        //Buraya Maps ctor metodu gelecektir.
+        //içine CreateMap metodu gelecektir
+
+        public Maps()
+        {
+
+            //UserAddress'ı UsersAddressesVm'ye dönüştür
+            //CreateMap<UsersAddress, UsersAddressVM>();   //DAL--> BLL
+
+            //UsersAddressVm'ı UserAddress'ye dönüştür
+            //CreateMap<UsersAddressVM, UsersAddress>();   //PL-->BLL-->DAL
+
+            //Yukardakilerinin aynısını tek seferde yapmak
+            //UserAddress ve VM'yi birbirine dönüştür
+            CreateMap<UsersAddress, UsersAddressVM>().ReverseMap();
+        }
+    }
+}
