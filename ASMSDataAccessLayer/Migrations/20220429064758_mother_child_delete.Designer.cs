2103785529da5b7c987c3a4aaff531075303adcd
Enes Çetintaş <enesctnts@gmail.com>
2022-04-29 12:31:26 +0300

    Mother ve Child sınıfları silindi ve veritabanına bildirildi.

--- ASMSDataAccessLayer/Migrations/20220429064758_mother_child_delete.Designer.cs
+++ ASMSDataAccessLayer/Migrations/20220429064758_mother_child_delete.Designer.cs
@@ -4,14 +4,16 @@ using ASMSDataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASMSDataAccessLayer.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20220429064758_mother_child_delete")]
+    partial class mother_child_delete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ASMSDataAccessLayer/Migrations/20220429064758_mother_child_delete.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASMSDataAccessLayer.Migrations
+{
+    public partial class mother_child_delete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Childs");
+
+            migrationBuilder.DropTable(
+                name: "Mothers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mothers",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(type: "tinyint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    MotherName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mothers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Childs",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(type: "tinyint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChildName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    MotherId = table.Column<byte>(type: "tinyint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Childs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Childs_Mothers_MotherId",
+                        column: x => x.MotherId,
+                        principalTable: "Mothers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Childs_MotherId",
+                table: "Childs",
+                column: "MotherId");
+        }
+    }
+}
