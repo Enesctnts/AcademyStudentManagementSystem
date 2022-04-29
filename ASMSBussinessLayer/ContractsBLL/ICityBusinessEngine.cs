24beb61b7f1c01b6730b867d9c6a284c6a1584ec
Enes Çetintaş <enesctnts@gmail.com>
2022-04-29 15:01:20 +0300

    ICityBusinessEngine Servicesi yazıldı.

---
+++ ASMSBussinessLayer/ContractsBLL/ICityBusinessEngine.cs
@@ -0,0 +1,21 @@
+﻿using ASMSEntityLayer.Models;
+using ASMSEntityLayer.ResultModels;
+using ASMSEntityLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSBusinessLayer.ContractsBLL
+{
+    public interface ICityBusinessEngine
+    {
+        IResult Add(CityVM city);
+        IResult Delete(CityVM city);
+        IResult Update(CityVM city);
+        IDataResult<CityVM> GetById(int city);
+        IDataResult<ICollection<CityVM>> GetAll();
+        IDataResult<ICollection<CityVM>> GetFirstOrDefault();
+    }
+}
