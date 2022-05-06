fcbd7d6e7a7fa3144458ca5aacb376a8dd3bc32a
Enes Çetintaş <enesctnts@gmail.com>
2022-05-06 15:04:44 +0300

    StudentViewModel oluşturuldu.

---
+++ ASMSEntityLayer/ViewModels/StudentVM.cs
@@ -0,0 +1,22 @@
+﻿using ASMSEntityLayer.IdentityModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSEntityLayer.ViewModels
+{
+    public class StudentVM
+    {
+        
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "TCKimlik numarası 11 haneli olmalıdır!")]
+        public string TCNumber { get; set; }
+        public string UserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
+
+        //bu VM genişeletilebilir. Örneğin öğrencinin aldıgı kurslar, öğrencinin devamsızlık durumu (yoklama bilgiisi)
+
+    }
+}
