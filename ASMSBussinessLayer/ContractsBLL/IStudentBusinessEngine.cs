0e8b8a1e847696789c505cc0548c3918ee5e1596
Enes Çetintaş <enesctnts@gmail.com>
2022-05-06 15:00:19 +0300

    IStudentBusinessEngine ve StudentBusinessEngine projeye eklendi.

---
+++ ASMSBussinessLayer/ContractsBLL/IStudentBusinessEngine.cs
@@ -0,0 +1,15 @@
+﻿using ASMSEntityLayer.ResultModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASMSEntityLayer.ViewModels;
+
+namespace ASMSBusinessLayer.ContractsBLL
+{
+    public interface IStudentBusinessEngine
+    {
+        IResult Add(StudentVM student);
+    }
+}
