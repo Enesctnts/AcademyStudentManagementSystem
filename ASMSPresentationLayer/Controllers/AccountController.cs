6c0782ccf1764da4718d4e70233067aa1477afae
Enes Çetintaş <enesctnts@gmail.com>
2022-05-09 11:08:58 +0300

    Login işlemi için backend yazıldı.
    Şifremi unuttum işlemi için backend yazılmaya başlandı.

--- ASMSPresentationLayer/Controllers/AccountController.cs
+++ ASMSPresentationLayer/Controllers/AccountController.cs
@@ -9,9 +9,12 @@ using ASMSEntityLayer.ViewModels;
 using ASMSPresentationLayer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace ASMSPresentationLayer.Controllers
@@ -114,6 +117,130 @@ namespace ASMSPresentationLayer.Controllers
             }
         }
 
+        [HttpGet]
+        public IActionResult Login(string email)
+        {
+            LoginViewModel model = new LoginViewModel()
+            {
+                Email = email
+            };
+            return View(model);
+          
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                var user = await _userManager.FindByNameAsync(model.Email);
+                //Böyle bir kullanıcı var mı?
+                if (user==null)
+                {
+                    ModelState.AddModelError("", "Epostanız yada şifreniz hatalıdı! Tekrar deneyiniz!");
+                    return View();
+                }
+                //Kullanıcının parolası doğru mu?
+
+                //ToDo: son parametre bool lockoutOnFailure ile ilgili bu da kaç defa şifre girdiğinde sisteme belirli süre şifre girişi yapamasın bunun için.
+                var result = await _signInManager.PasswordSignInAsync(user, model.Password,model.RememberMe, false);
+
+                //ToDo: son parametre bool lockoutOnFailure ile ilgili bu da kaç defa şifre girdiğinde sisteme belirli süre şifre girişi yapamasın bunun için.
+                //Burda sistem lockoutOnFailure işlemi true mu false mu diye bakıyor. eger lockoutOnFailure true yaparsak şifreyi mesela 3 kez yanlış yazdıktan sonra belirli bir süre sisteme giriş yapamasın işleminin sonucunda sistem kilitli mi diye bakıyoruz.Sistem kilitliyse aşagıdaki hataayı verdircez
+                //if (result.IsLockedOut)
+                //{
+                //    DateTimeOffset d = user.LockoutEnd.Value;
+                //}
+
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", "Epostanız ya da şifreniz hatalıdır! Tekrar deneyiniz!");
+                    return View();
+                }
+                //artık hoş geldi
+                //Sisteme giren kişinin rolü Student ise Dashboard sayfası açılsın diyoruz.
+                if (_userManager.IsInRoleAsync(user,ASMSRoles.Student.ToString()).Result)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                //Sisteme giren kişinin rolü admin Dashboard sayfası açılsın diyoruz.
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.Coordinator.ToString()).Result)
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+                //Sisteme giren kişinin rolü öğrenci işleri Dashboard sayfası açılsın diyoruz.
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.StudentAdministration.ToString()).Result)
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Beklenmedik bir hata oluştu! Tekrar deneyiniz");
+
+                //ex loglanacak
+                return View(model);
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(string email)
+        {
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    ViewBag.ResetPasswordMessage = "Şifre yenileme talebiniz alındı! Epostanızı kontrol ediniz!";
+                    return View();
+                }
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var codeEncode = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callBackUrl = Url.Action("ConfirmResetPassword", "Account", new { userId = user.Id, code = codeEncode }, protocol: Request.Scheme);
+                var emailMessage = new EmailMessage()
+                {
+                    Contacts = new string[] { user.Email },
+                    Subject="ASMS - Yeni Şifre Talebi",
+                    Body=$"Merhaba {user.Name} {user.Surname},"  +
+                    $"<br/> Yeni parola belirlemek için" +
+                    $"<a href='{HtmlEncoder.Default.Encode(callBackUrl)}'> buraya </a> tıklayınız..."
+
+                };
+
+                await _emailSender.SendMessage(emailMessage);
+                ViewBag.ResetPasswordSuccessMessage = "Şifre yenileme talebiniz alındı! Epostanızı kontrol ediniz!";
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                // ex loglanacak
+                ViewBag.ResetPasswordFailMessage = "Beklenmedik bir hata oluştu! Tekrar deneyiniz!";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ConfirmResetPassword(string userId, string code)
+        {
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(code))
+            {
+                ViewBag.ConfirmResetPasswordFailureMessage = "Beklenmedik bir hata oluştu!";
+                return View();
+            }
+        }
     }
 }
