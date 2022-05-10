3163fc7da0bff32d316b905a30cd3ba4cb1ab3c1
Enes Çetintaş <enesctnts@gmail.com>
2022-05-10 12:23:38 +0300

    RegisterViewModel düzenlendi.

--- ASMSPresentationLayer/Models/RegisterViewModel.cs
+++ ASMSPresentationLayer/Models/RegisterViewModel.cs
@@ -12,21 +12,25 @@ namespace ASMSPresentationLayer.Models
 
         [Required]
         [StringLength(11, MinimumLength = 11, ErrorMessage = "TC 11 haneli olmalıdır!")]
+        [Display(Name ="Tc Kimlik Numarası")]
         public string TCNumber { get; set; }
 
 
         [Required(ErrorMessage = "İsim Gereklidir!")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "İsminiz en az 2 en çok 50 karakter olmalıdır!")]
+        [Display(Name = "İsim")]
         public string Name { get; set; }
 
 
         [Required(ErrorMessage = "Soyisim Gereklidir!")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Soyisminiz en az 2 en çok 50 karakter olmalıdır!")]
+        [Display(Name = "Soyisim")]
         public string Surname { get; set; }
 
 
         [Required(ErrorMessage ="Email Zorunludur!")]
         [EmailAddress]
+        [Display(Name = "Mail Adresi")]
         public string Email { get; set; }
 
 
@@ -38,10 +42,12 @@ namespace ASMSPresentationLayer.Models
 
 
         [DataType(DataType.Date)]
+        [Display(Name = "Doğum Tarihi")]
         public DateTime? BirthDate { get; set; }
 
 
         [Required(ErrorMessage ="Cinsiyet Seçimi Gereklidir1")]
+        [Display(Name = "Cinsiyetiniz")]
         public Genders Gender { get; set; }
 
     }
